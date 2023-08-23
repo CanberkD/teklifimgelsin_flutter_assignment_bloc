9dbece71a6f531364427a5b59279c96510fd4457
CanberkD <canberkkdoner@gmail.com>
2023-08-23 09:48:30 +0300

    Core files added.

---
+++ lib/product/constants/design/design_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import '../theme/project_colors.dart';
+
+class DesignConstants{
+  static final Border defaultGreyBorder = Border.all(width: 0.2, color: ProjectColors.inactiveGrey);
+  
+}
