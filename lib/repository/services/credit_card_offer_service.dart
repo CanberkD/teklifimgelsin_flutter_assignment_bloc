94bc5e3a16efa5e08592a17299b01d2090c362a5
CanberkD <canberkkdoner@gmail.com>
2023-09-02 00:38:18 +0300

    Credit card offer service postModel fix.

--- lib/repository/services/credit_card_offer_service.dart
+++ lib/repository/services/credit_card_offer_service.dart
@@ -6,26 +6,14 @@ import 'package:teklifimgelsin_flutter_assignment_bloc/repository/models/credit-
 
 class CreditCardOfferService{
   Future<GetOffersResponseModel> getOffers(GetOffersPostModel postModel) async {
-    GetOffersPostModel getOffersPostModel = GetOffersPostModel(
-      age: 4,
-      bill: 8,
-      dining: 5,
-      grocery: 1,
-      installment: 5,
-      mile: 2,
-      onlineShopping: 3, 
-      other: 13, 
-      point: 3, 
-      saleCashback: 1, 
-      travel: 2
-    );
+
     GetOffersResponseModel getOffersResponseModel = GetOffersResponseModel();
     
     //Create serviceManager.
     AServiceManager serviceManager = DioServiceManager();
 
     //Send post request with post model and add results response model.
-    await serviceManager.instance?.post(ServicePaths().getCardOffers(), getOffersPostModel, getOffersResponseModel, null, null, null);
+    await serviceManager.instance?.post(ServicePaths().getCardOffers(), postModel, getOffersResponseModel, null, null, null);
 
     return getOffersResponseModel;
   }
